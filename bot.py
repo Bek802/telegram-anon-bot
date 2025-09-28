51204523f15183b0790dcbcd1999f0eb7f14cf93
Bek802 <karimovbekzod0517@gmail.com>
2025-09-28 21:46:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
+from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ContextTypes
 
 TOKEN = "8349538809:AAH99J3Wa0tR3WmEDqTrlXmEy-NpSDoA0MQ"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salom! Men yangi Telegram botman 🤖")
+    await update.message.reply_text("Salom! Men anonim xabar botman.")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
+
     app.run_polling()
 
 if __name__ == "__main__":
     main()
+
